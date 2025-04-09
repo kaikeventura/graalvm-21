66769a62d765106fc67a807f66b83e71bafb1d8f
kaikeventura <kaike.gnr@hotmail.com>
2025-04-09 20:36:48 -0300

    first commit

---
+++ src/main/java/com/kaikeventura/graalvm_21/Graalvm21Application.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.graalvm_21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Graalvm21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Graalvm21Application.class, args);
+	}
+
+}
